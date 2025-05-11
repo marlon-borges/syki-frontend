bfe71a27ee4208656858deb7ec33ae254dde07aa
Marlon Daniel <marlondaniel.art@gmail.com>
2025-05-11 13:09:14 -0300

    feat: QueryClientProvider via Tanstack

--- src/index.tsx
+++ src/index.tsx
@@ -9,34 +9,48 @@ import { LoginPage } from '@/pages/public/Login/LoginPage';
 import { RegisterPage } from '@/pages/public/Register/RegisterPage';
 import { ResetPasswordPage } from '@/pages/public/ResetPassword/ResetPasswordPage';
 import { ProtectedLayout } from '@/components/ProtectedLayout';
+import { CookiesProvider } from 'react-cookie';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const rootEl = document.getElementById('root');
 if (rootEl) {
    const root = ReactDOM.createRoot(rootEl);
    root.render(
       <React.StrictMode>
-         <BrowserRouter>
-            <Routes>
-               /* Public */
-               <Route path="/login" element={<LoginPage />} />
-               <Route path="/register" element={<RegisterPage />} />
-               <Route path="/reset-password" element={<ResetPasswordPage />} />
-               /* Protected */
-               <Route path="/academic" element={<ProtectedLayout />}>
-                  <Route index element={<Navigate to="campi" replace />} />
-                  {AcademicRoutes()}
-               </Route>
-               <Route path="/teacher" element={<ProtectedLayout />}>
-                  <Route index element={<Navigate to="home" replace />} />
-                  {TeacherRoutes()}
-               </Route>
-               <Route path="/student" element={<ProtectedLayout />}>
-                  <Route index element={<Navigate to="home" replace />} />
-                  {StudentRoutes()}
-               </Route>
-               <Route path="*" element={<h1>Não autorizado</h1>} />
-            </Routes>
-         </BrowserRouter>
+         <QueryClientProvider client={queryClient}>
+            <CookiesProvider>
+               <BrowserRouter>
+                  <Routes>
+                     /* Public */
+                     <Route path="/login" element={<LoginPage />} />
+                     <Route path="/register" element={<RegisterPage />} />
+                     <Route
+                        path="/reset-password"
+                        element={<ResetPasswordPage />}
+                     />
+                     /* Protected */
+                     <Route path="/academic" element={<ProtectedLayout />}>
+                        <Route
+                           index
+                           element={<Navigate to="campi" replace />}
+                        />
+                        {AcademicRoutes()}
+                     </Route>
+                     <Route path="/teacher" element={<ProtectedLayout />}>
+                        <Route index element={<Navigate to="home" replace />} />
+                        {TeacherRoutes()}
+                     </Route>
+                     <Route path="/student" element={<ProtectedLayout />}>
+                        <Route index element={<Navigate to="home" replace />} />
+                        {StudentRoutes()}
+                     </Route>
+                     <Route path="*" element={<h1>Não autorizado</h1>} />
+                  </Routes>
+               </BrowserRouter>
+            </CookiesProvider>
+         </QueryClientProvider>
       </React.StrictMode>,
    );
 }
